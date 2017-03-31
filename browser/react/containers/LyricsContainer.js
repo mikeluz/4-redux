03bc17ea02f82131a2a2b5c184bb0b0998f1b8a1
David R Lowry Lowry <ross@Davids-MacBook-Pro.local>
2017-03-31 14:38:54 -0400

    after lunch

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Lyrics from '../components/Lyrics';
 import store from '../store';
+import setLyrics from '../action-creators/lyrics'
+import axios from 'axios'
 
 class LyricsContainer extends React.Component {
 
 	constructor () {
-		this.state = store.getState();
+		super();
+		this.state = Object.assign({
+			artistQuery: '',
+			songQuery: ''
+		}, store.getState());
+
+		this.handleArtistInput = this.handleArtistInput.bind(this);
+	    this.handleSongInput = this.handleSongInput.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+
 	}
 
 	ComponentDidMount () {
@@ -18,11 +29,41 @@ class LyricsContainer extends React.Component {
 		this.unsubscribe();
 	}
 
+	handleArtistInput(artist) {
+		this.setState({artistQuery: artist});
+	}
+
+	handleSongInput(song) {
+		this.setState({ songQuery: song});
+	}
+
+	handleSubmit() {
+		event.preventDefault();
+ 		   if (this.state.artistQuery && this.state.songQuery) {
+
+	      axios.get(`/api/lyrics/${this.state.artistQuery}/${this.state.songQuery}`)
+	        .then(response => {
+	          const setLyricsAction = setLyrics(response.data.lyric);
+	          store.dispatch(setLyricsAction);           
+	      });
+
+    }
+
+
+	}
+
+
+
+
 	render () {
 		return (
-
 			<Lyrics
-				
+		      text={this.state.text}
+		      setArtist={this.handleArtistInput}
+		      setSong={this.handleSongInput}
+		      artistQuery={this.state.artistQuery}
+		      songQuery={this.state.songQuery}
+		      handleSubmit={this.handleSubmit}
 			/>
 
 		);
--- browser/react/store.js
+++ browser/react/store.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import reducer from './reducers/root-reducer';
 
 export default createStore(reducer, null, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+var createLoggerResult = createLogger()
+var applyMiddlewareResult = applyMiddleware(createLoggerResult)
